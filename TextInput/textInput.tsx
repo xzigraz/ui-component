099071696c2ac136a9cfb8d49c719fb8304b54ba
Daniel Zhu <daniel@inxeption.com>
2024-08-26 12:49:02 -0700

    Update text input component.
    Add button css.

--- TextInput/textInput.tsx
+++ TextInput/textInput.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import clsx from "clsx";
 import "./text.input.scss";
+import Asterisk from "../svgs/asterisk";
 
 interface TextInputProps {
 	name: string
@@ -10,6 +11,8 @@ interface TextInputProps {
 	min?: string
 	max?: string
 	isRequired?: boolean
+	isTextArea?: boolean
+	textAreaRows?: number
 	placeholder?: string
 	onValueChange: (value: string) => void
 }
@@ -22,17 +25,22 @@ export const TextInput = ({
 	min, 
 	max, 
 	isRequired = false, 
+	isTextArea = false,
+	textAreaRows = 4,
 	onValueChange, 
 	...props}: TextInputProps) => {
 	const [value, setValue] = useState<string>("");
 
-	const handleValueChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+	const handleValueChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
 		setValue(e.target.value);
 		onValueChange(e.target.value);
 	}
 
-	return <div className={clsx("td-input-container", isLabelCapitalized && "cap-label")}>
-		{label && <label>{label}</label>}
-		<input name={name} type={type} placeholder={props.placeholder} onChange={(e) => handleValueChange(e)} value={value}/>
+	return <div className={clsx("td-input-container", isLabelCapitalized && "cap-label", isRequired && "is-required")}>
+		{label && <label>{isRequired && <Asterisk />}{label}</label>}
+		{isTextArea 
+			? <textarea name={name} placeholder={props.placeholder} rows={textAreaRows} onChange={(e) => handleValueChange(e)} value={value}/>
+			: <input name={name} type={type} placeholder={props.placeholder} onChange={(e) => handleValueChange(e)} value={value}/>
+		}
 	</div>
 }
